faa880756c38e1e95d2c5fcbe964dc2c074b96f6
SerNikif <170589546+SerNikif@users.noreply.github.com>
2024-06-04 21:40:34 +0800

    Add files via upload

---
+++ Basic data structures.py
@@ -0,0 +1,19 @@
+name = 'Сергей Никифоров'
+print(len(name))
+first = 10
+second = 5
+print(first + second)
+print(first - second)
+first = 10
+second = 5
+third = 15
+mean = (first + second + third) // 3
+print(mean)
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string , first_string)
+a = 3
+b = 4
+c = 5
+f = (((a * b) + (a * c)) ** 3) / 2
+print(f)
