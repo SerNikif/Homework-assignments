59c7712bf54d950598348ac62e9ffca04446afbc
SerNikif <170589546+SerNikif@users.noreply.github.com>
2024-06-14 09:51:24 +0800

    homework3

--- homework3.py
+++ homework3.py
@@ -2,7 +2,7 @@ name = 'Sergei'
 print('Name:',name)
 age = 52
 print('Age:', age)
-new_age = age + 10
-print('New Age:', new_age)
+age = age + 10
+print('New Age:', age)
 is_student = True
 print('Is_studentis:', is_student)
