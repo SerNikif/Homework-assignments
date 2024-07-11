0dff1843bc4e9d2252002b207806b69d2c2864e2
SerNikif <170589546+SerNikif@users.noreply.github.com>
2024-07-11 17:54:27 +0800

    Add files via upload

---
+++ module_5_3.history.py
@@ -0,0 +1,34 @@
+class House:
+
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа несуществует')
+        for i in range(new_floor):
+            j = i + 1
+            if new_floor < self.number_of_floors:
+                print(j)
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+del h2
+del h3
+print(House.houses_history)
+
