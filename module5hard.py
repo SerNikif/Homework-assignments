d04b31f644cf64b39bd089364e6a0fe39f215937
SerNikif <170589546+SerNikif@users.noreply.github.com>
2024-07-22 09:02:43 +0800

    Add files via upload

--- module5hard.py
+++ module5hard.py
@@ -1,84 +1,84 @@
 import time
 
 
+class User:
+    def __init__(self, nickname: str, password: int, age: int):
+        self.nickname = nickname
+        self.password = password
+        self.age = age
+
+    def __str__(self):
+        return self.nickname
+
+
+class Video:
+    def __init__(self, title: str, duration: int, adult_mode: bool = False):
+        self.title = title
+        self.duration = duration
+        self.time_now = 0
+        self.adult_mode = adult_mode
+
+    def __eq__(self, other):
+        return self.title == other.title
+
+    def __contains__(self, item):
+        return item in self.title
+
+
 class UrTube:
-    users = []
-    videos = []
-    current_user = None
+    def __init__(self):
+        self.users = []
+        self.videos = []
+        self.current_user = None
 
-    def log_in(self, login: str, password: str) -> None:
+    def register(self, nickname: str, password: str, age: int):
+        password = hash(password)
         for user in self.users:
-            if login == user.nickname and password == user.password:
-                self.current_user = user
-
-    def register(self, nickname: str, password: str, age: int) -> None:
-        for user in self.users:
-            if nickname in user.nickname:
+            if user.nickname == nickname:
                 print(f"Пользователь {nickname} уже существует")
-                break
-        else:
-            user = User(nickname, password, age)
-            self.users.append(user)
-            self.log_out()
-            self.log_in(user.nickname, user.password)
+                return
+
+        new_user = User(nickname, password, age)
+        self.users.append(new_user)
+        self.current_user = new_user
 
     def log_out(self):
         self.current_user = None
 
+    def log_in(self, login: str, password: str):
+        for user in self.users:
+            if login == user.nickname and password == user.password:
+                self.current_user = user
+
     def add(self, *args):
         for movie in args:
-            self.videos.append(movie)
+            if movie not in self.videos:
+                self.videos.append(movie)
 
-    def get_videos(self, text: str) -> list:
-
+    def get_videos(self, text: str):
         list_movie = []
         for video in self.videos:
             if text.upper() in video.title.upper():
                 list_movie.append(video.title)
         return list_movie
 
-    def watch_video(self, movie: str) -> None:
-        if self.current_user and self.current_user.age < 18:
-            print('Вам нет 18 лет, пожалуйста покиньте страницу')
-        elif self.current_user:
-            for video in self.videos:
-                if movie in video.title:
-                    for i in range(1, 11):
-                        print(i, end=' ')
-                        time.sleep(1)
-                    print('Конец видео')
-        else:
+    def watch_video(self, movie: str):
+        if not self.current_user:
             print('Войдите в аккаунт, чтобы смотреть видео')
+            return
 
-    def __str__(self):
-        return f"{self.videos}"
+        for x in self.videos:
+            if x.title == movie:
+                if x.adult_mode and self.current_user.age < 18:
+                    print('Вам нет 18 лет, пожалуйста, покиньте страницу')
+                    return
 
-
-class Video:
-    def __init__(self, title: str, duration: int, time_now: int = 0, adult_mode: bool = False):
-        self.title = title
-        self.duration = duration
-        self.time_now = time_now
-        self.adult_mode = adult_mode
-
-    def __str__(self):
-        return f"{self.title}"
-
-
-class User:
-    def __init__(self, nickname: str, password: str, age: int):
-        self.nickname = nickname
-        self.password = password
-        self.age = age
-
-    def __str__(self):
-        return f'{self.nickname}'
-
-    def __eq__(self, other):
-        return self.nickname == other.nickname
-
-    def __hash__(self):
-        return hash(self.password)
+                for i in range(1, 11):
+                    print(i, end=' ')
+                    x.time_now += 1
+                    x.time_now = 0
+                    time.sleep(1)
+                print('Конец видео')
 
 
 if __name__ == '__main__':
