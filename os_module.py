82c26ab6aca7c57abf4b362c8da311a56adee918
SerNikif <170589546+SerNikif@users.noreply.github.com>
2024-09-16 11:35:14 +0800

    Add files via upload

--- os_module.py
+++ os_module.py
@@ -1,7 +1,6 @@
 import os
 import time
 
-
 directory = '.'
 
 for root, dirs, files in os.walk(directory):  # для просмотра вложенных директорий ( '.' означает текущая директория)
@@ -11,6 +10,5 @@ for root, dirs, files in os.walk(directory):  # для просмотра вло
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime())
         file_size = os.path.getsize(file)
         parent_dir = os.path.dirname(file)
-        abs_path = os.path.abspath(file)
-        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {file_size} байт, Время изменения: {formatted_time}, '
-              f'Родительская директория: {parent_dir}, Абсолютный путь: {abs_path}')
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Время изменения: {formatted_time}, Размер: {file_size} байт,'
+              f' Родительская директория: {parent_dir}')
