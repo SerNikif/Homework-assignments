79ce3bcff0472d9ee563cb9858fc4f5e712411ab
SerNikif <170589546+SerNikif@users.noreply.github.com>
2024-11-15 09:22:46 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -10,6 +10,4 @@ print(9.99 > 9.98 and 1000 != 1000.1)
 print((2 * 2 + 2) == (2 * (2 + 2)))
 
 # 4st program
-a = '123.456'
-b = float(a) * 10
-print(int(b) % 10)
+print(int(float('123.456') * 10) % 10)
