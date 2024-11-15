fcb0e068d4995afb58728ebf66f5ca5d297ca156
SerNikif <170589546+SerNikif@users.noreply.github.com>
2024-11-15 09:20:20 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+print(12 + 89 - 1)
+
+# 1st program
+print(int(9 ** 0.5 * 5))
+
+# 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3st program
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4st program
+a = '123.456'
+b = float(a) * 10
+print(int(b) % 10)
